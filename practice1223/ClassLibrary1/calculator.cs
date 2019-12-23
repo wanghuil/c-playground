2cbd112b3a3eed0fbd30483a45513bcd3e95dc50
wanghuil <33080153+wanghuil@users.noreply.github.com>
2019-12-23 17:06:18 +1030

    Add files via upload

---
+++ practice1223/ClassLibrary1/calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuperCalculator
+{
+    public class calculator
+    {
+        public static double Add(double a, double b) { return a + b; }
+
+        public static double Sub(double a, double b) { return a - b; }
+
+        public static double Mul(double a, double b) { return a * b; }
+
+        public static double Div(double a, double b) { 
+            if (b==0) { return double.PositiveInfinity; }
+            else { return a / b; }
+        }
+
+    }
+}
