2d0c6e97cb00f1cacb3b0f6f9e65e43252da2a98
Wanda <33080153+wanghuil@users.noreply.github.com>
2020-01-03 16:19:26 +1030

    Add files via upload

---
+++ EventExample/EventExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            Boy boy = new Boy();
+            timer.Elapsed += boy.Action; //1 event with 2 event handlers
+            timer.Elapsed += Girl.Action;
+            timer.Start();
+            Console.ReadLine();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Boy
+    {
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Jump!");
+        }
+    }
+
+    class Girl
+    {
+        internal static void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Sing!");
+        }
+    }
+}
---
+++ EventExampleForm/EventExampleForm/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventExampleForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyForm form = new MyForm();
+            //form.Click += form.FormClicked;
+            form.ShowDialog();
+        }
+    }
+
+    class MyForm : Form
+    {
+        private TextBox textbox;
+        private Button button;
+
+        public MyForm()
+        {
+            this.textbox = new TextBox();
+            this.button = new Button();
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textbox);
+            this.button.Click += this.ButtonClicked;
+            this.button.Text = "Say Hello";
+            this.button.Top = 100;
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            this.textbox.Text = "Hello World";
+        }
+
+        //internal void FormClicked(object sender, EventArgs e)
+        //{
+        //    this.Text = DateTime.Now.ToString();
+        //}
+    }
+}
