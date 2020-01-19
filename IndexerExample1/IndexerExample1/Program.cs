86b1c4e6346599625274bbb24c355f8960cbd462
wanghuil <wanghuil@andrew.cmu.edu>
2020-01-19 22:49:22 +1030

    add indexer example

---
+++ IndexerExample1/IndexerExample1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexerExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            stu["Math"] = 90;
+            var mathScore = stu["Math"];
+            Console.WriteLine(mathScore );
+            Console.WriteLine(URL.url);
+            Console.WriteLine(URL.Location.Address);
+        }
+    }
+
+    class Student
+    {
+        private Dictionary<string, int> scoreDictionary = new Dictionary<string, int>();
+
+        public int? this[string subject]
+        {
+            get
+            {
+                if (this.scoreDictionary.ContainsKey(subject))
+                {
+                    return this.scoreDictionary[subject];
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+
+            set
+            {
+                if (this.scoreDictionary.ContainsKey(subject))
+                {
+                    this.scoreDictionary[subject] = value.Value;
+                }
+                else
+                {
+                    this.scoreDictionary.Add(subject, value.Value);
+                }
+            }
+        }
+
+    }
+
+    class URL
+    {
+        public const string url ="http://www.aaa.com.au";
+        public static readonly Building Location = new Building("some addresses");
+    }
+
+    class Building
+    {
+        public Building(string address)
+        {
+            this.Address = address;
+        }
+        public string Address { get; set; }
+
+    }
+}
