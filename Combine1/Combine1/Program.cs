62fb5fb96a843cf6ab3dd573221ce53177210c83
wanghuil <wanghuil@andrew.cmu.edu>
2020-01-20 16:36:22 +1030

    add combine and linq examples

---
+++ Combine1/Combine1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Combine1.Models;
+
+namespace Combine1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //linq
+            var dbContext = new AdventureWorks2014Entities();
+            var allFirstNames = dbContext.People.Select(p => p.FirstName).ToList();
+            var allFullNames = dbContext.People.Select(p => p.FirstName + " " + p.LastName).ToList();
+            var allTims = dbContext.People.Where(p => p.FirstName == "Tim")
+                .Select(p => p.FirstName + " " + p.LastName).ToList();
+            var boolTim = dbContext.People.Any(p => p.FirstName == "Tim");
+            var groups = dbContext.People.GroupBy(p => p.FirstName).ToList();
+            foreach (var g in groups)
+            {
+                Console.WriteLine("Name:{0}\tCount:{1}", g.Key, g.Count());
+            }
+
+            var count = dbContext.People.Count(p => p.FirstName == "Tim");
+            Console.WriteLine(count);
+            //var allPeople = dbContext.People.ToList();
+            //foreach (var p in allPeople)
+            //{
+            //    Console.WriteLine(p.FirstName);
+            //}
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
